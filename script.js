1fc231a38b71a92c4eb13cd89d00471855e13822
T3s0 <ylepe@amityteso.com>
2025-08-22 01:47:55 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("clickMe");
+  const output = document.getElementById("output");
+
+  button.addEventListener("click", () => {
+    const now = new Date().toLocaleString();
+    output.textContent = `You clicked the button at ${now}`;
+  });
+});
