043fee10e3944940260aa20ccff13064afee3ddb
T3s0 <ylepe@amityteso.com>
2025-08-22 02:09:24 -0700

    purge test

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("clickMe");
   const output = document.getElementById("output");
 
-  button.addEventListener("click", () => {
+  button.addEventListener("click-click-clicky button", () => {
     const now = new Date().toLocaleString();
     output.textContent = `You clicked the button at ${now}`;
   });
