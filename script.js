6d99f6a9fb7602408df49a6b3236a2791a885291
T3s0 <ylepe@amityteso.com>
2025-08-22 03:32:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("clickMe");
   const output = document.getElementById("output");
 
-  button.addEventListener("clickMe", () => {
+  button.addEventListener("click", () => {
     const now = new Date().toLocaleString();
     output.textContent = `You clicked the click-click-clicky button at: ${now}`;
   });
